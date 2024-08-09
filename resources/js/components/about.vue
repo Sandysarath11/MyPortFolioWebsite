d7ab91bc95991a90154f865ef20364abf5fed068
sarathkumar <sarathkumar@onemodo.com>
2024-08-09 11:24:56 +0530

    about almost done pending for skills added.

--- resources/js/components/about.vue
+++ resources/js/components/about.vue
@@ -1,3 +1,44 @@
 <template>
-    <h1>About Page!</h1>
+    <div class="container-fluid" style="padding-top:8% ">
+        <div class="row">
+            <div class="col-md-6">
+
+            </div>
+            <div class="col-md-6">
+                <div style="font-size: 50PX;font-weight: bold;font-family: sans-serif;">MY BIOGRAPHY</div>
+                <hr width="390" style="height: 3%;">
+                <div class="row mt-4">
+                    <div class="col-md-8">
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sequi natus voluptate sunt at facilis alias impedit debitis enim amet, rerum mollitia iste perferendis sint consequatur voluptas eos. Repudiandae, quo.lo Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, sint earum. Veniam, velit quisquam harum aliquid amet maiores, impedit magnam quaerat a minus perferendis dolorum est, consectetur laborum eveniet voluptate!
+                        </div>
+                        <div class="mt-4">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab minima eaque maxime sed aut, distinctio nam asperiores ducimus quam dolore, fugiat excepturi sit quis autem expedita vel! Magnam, laborum aliquam?
+                        </div>
+                        <div class="mt-4">
+                            <div style="font-size: 20px;font-weight: bold;padding-top: 3%;">Skills & Interest</div>
+                            <div class="mt-2">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="border rounded-pill text-center" style="width: 180px;height: 50px;">
+                                            <p style="font-size: 20px;margin-top: 8px;">HTML</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="border rounded-pill text-center" style="width: 180px;height: 50px;">
+                                            <p style="font-size: 20px;margin-top: 8px;">CSS</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="border rounded-pill text-center" style="width: 180px;height: 50px;">
+                                            <p style="font-size: 20px;margin-top: 8px;">JS</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
